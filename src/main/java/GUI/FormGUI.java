457189bcf1709d72dcb437774cc354770b0c7607
Aleksander <olek230303@gmail.com>
2023-03-09 23:21:47 +0100

    adding new form to the GUI package

--- src/main/java/GUI/FormGUI.java
+++ src/main/java/GUI/FormGUI.java
@@ -5,37 +5,42 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MyGUI extends JFrame {
-    public MyGUI() {
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel("Welcome to the car insurance app");
-        JButton button = new JButton("Let's Start");
+public class FormGUI extends JFrame{
+    public FormGUI() {
         setTitle("Car Insurance Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setSize(1000,720);
         getContentPane().setBackground(Color.BLACK);
 
+        JLabel text = new JLabel("Please enter your login to continue");
+        JPanel panel = new JPanel();
+        JTextField login = new JTextField(20);
+        JButton button = new JButton("Confirm");
+
         panel.setBackground(Color.BLACK);
         //panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        label.setForeground(Color.WHITE);
-        label.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 32));
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 32));
 
         button.setBounds(100,500,95,30);
-        button.setBackground(Color.WHITE);
+        button.setBackground(Color.GREEN);
+
         panel.add(Box.createRigidArea(new Dimension(1000,100)));
-        panel.add(label, BorderLayout.CENTER);
-        panel.add(Box.createRigidArea(new Dimension(1000,10)));
+        panel.add(text, BorderLayout.CENTER);
+        panel.add(Box.createRigidArea(new Dimension(1000,5)));
+        panel.add(login, BorderLayout.CENTER);
+        panel.add(Box.createRigidArea(new Dimension(1000,5)));
         panel.add(button, BorderLayout.CENTER);
         panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         add(panel);
+
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(getComponent(0), "Welcome to the car insurance calculator. Enter your login.");
-                new FormGUI().setVisible(true);
-                setVisible(false);
+
             }
         });
+
     }
 }
