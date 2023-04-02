bf4554e0348068a2ed50a2f9533a129a93d7d7fd
olek2303 <olek230303@gmail.com>
2023-04-02 23:03:07 +0200

    finally working web scraping for car brands, need to complete the rest of web scraping

--- src/main/java/core/webScrap.java
+++ src/main/java/core/webScrap.java
@@ -15,16 +15,14 @@ public class webScrap {
     public static void getBrandsModel() throws IOException {
         String url = "https://www.autocentrum.pl/dane-techniczne/";
         Document document = Jsoup.connect(url)
-                .userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
-                .timeout(5000)
-                .cookie("cookiename", "val234")
-                .referrer("https://google.com")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
                 .get();
 
-        Elements brands = document.select("div.popular-name");
+        Elements brands = document.select("div.popular-make");
         for(Element e : brands.select("a.make")) {
             String s = e.select("span").text();
             System.out.println(s);
         }
+        System.out.print("\nBrands: " + brands.size());
     }
 }
