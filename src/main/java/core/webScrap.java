c94d02535ada8df487317b9ec2235b315421a3ef
olek2303 <olek230303@gmail.com>
2023-04-03 13:48:03 +0200

    web scarping for scraping models and car brands, need to implement in gui

--- src/main/java/core/webScrap.java
+++ src/main/java/core/webScrap.java
@@ -3,26 +3,49 @@ package core;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.*;
 
 public class webScrap {
     public static void main(String[] args) throws IOException {
-        getBrandsModel();
+        getModels("audi");
     }
 
-    public static void getBrandsModel() throws IOException {
+    public static List<String> getBrandsModel() throws IOException {
         String url = "https://www.autocentrum.pl/dane-techniczne/";
         Document document = Jsoup.connect(url)
                 .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
                 .get();
 
         Elements brands = document.select("div.popular-make");
+        List<String> b = new ArrayList<>();
         for(Element e : brands.select("a.make")) {
             String s = e.select("span").text();
-            System.out.println(s);
+            s = s.replace(" ", "-").toLowerCase();
+            b.add(s);
+            //System.out.println(s);
         }
-        System.out.print("\nBrands: " + brands.size());
+        //System.out.print("\nBrands: " + brands.size() + " " + b.size());
+        return b;
+    }
+
+    public static List<String> getModels(String brand) throws IOException {
+        String url = "https://www.autocentrum.pl/dane-techniczne/";
+        url = url + brand + "/";
+        Document doc = Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+                .get();
+        Elements models = doc.select("a.big-box");
+        List<String> m = new ArrayList<>();
+        for(Element e : models) {
+            String s = e.select("h2.name-of-the-car").text();
+            s = s.replace(" ", "-").toLowerCase();
+            m.add(s);
+            //System.out.println(s);
+        }
+        return m;
     }
 }
