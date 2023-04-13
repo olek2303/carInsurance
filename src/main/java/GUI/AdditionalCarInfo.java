e5a3c68698d752a48ba9b7be65279d97d400601b
olek2303 <olek230303@gmail.com>
2023-04-13 20:36:51 +0200

    briefly ended gui, need to check everything and continue with repairing webscrap file and core

--- src/main/java/GUI/AdditionalCarInfo.java
+++ src/main/java/GUI/AdditionalCarInfo.java
@@ -6,15 +6,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.List;
 import java.util.Vector;
+
+import static java.awt.Font.*;
 
 public class AdditionalCarInfo extends JPanel {
     private JLabel text = new JLabel("Additional Data About Car:");
     private JToggleButton OCAC = new JToggleButton("Select OCAC (default OC): ");
-    private List<JTextField> intCollector = new Vector<>(6);
+    private List<JTextField> intCollector = new Vector<>();
     private List<JLabel> labels = new Vector<>();
     private JTextField parking = new JTextField();
     private JButton button = new JButton("Confirm");
@@ -22,21 +22,42 @@ public class AdditionalCarInfo extends JPanel {
     public AdditionalCarInfo() {
         setBackground(Color.BLACK);
         text.setForeground(Color.WHITE);
-        text.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 32));
-        add(OCAC);
+        text.setFont(new Font(SANS_SERIF, BOLD, 32));
         labels.add(new JLabel("Amount of doors: "));
         labels.add(new JLabel("Current counter status: "));
         labels.add(new JLabel("Kilometers in Year: "));
         labels.add(new JLabel("Years of Ownership: "));
         labels.add(new JLabel("Years of OC: "));
         labels.add(new JLabel("Years without accident: "));
+        for(int i = 0; i < labels.size(); i++) {
+            intCollector.add(new JTextField());
+        }
+        JLabel text = new JLabel("Additional Data About Car");
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font(SANS_SERIF, BOLD, 32));
+        add(Box.createRigidArea(new Dimension(1000,10)));
+        add(text);
+        add(Box.createRigidArea(new Dimension(1000,10)));
+        add(OCAC);
         for(JLabel txt : labels) {
             txt.setForeground(Color.WHITE);
-            txt.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 16));
+            txt.setFont(new Font(MONOSPACED, PLAIN, 16));
         }
         for(JTextField i : intCollector) {
             i.setColumns(20);
         }
+        for(int i = 0; i < labels.size(); i++) {
+            add(Box.createRigidArea(new Dimension(1000,10)));
+            add(labels.get(i));
+            add(intCollector.get(i));
+        }
+
+        button.setPreferredSize(new Dimension(150,30));
+        button.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+        button.setBackground(Color.GREEN);
+
+        add(Box.createRigidArea(new Dimension(1000,10)));
+        add(button);
         
         button.addActionListener(new ActionListener() {
             @Override
